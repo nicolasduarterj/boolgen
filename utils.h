dacff3d3ab4d079b27a7915e7b7e436005d49ef3
nicolasduarterj <nicolas_duarterj@proton.me>
2024-06-24 16:15:38 -0300

    Update descrição do xorfilter

--- utils.h
+++ utils.h
@@ -2,6 +2,7 @@
 #define BOOLGEN_UTILS_H
 #include <stdbool.h>
 
+//! Warning: will filter only 1 XOR operation per call!
 char *xorfilter(char *syntax);
 // entrypoint = pointer to the opening parentheses; direction = 'l' or 'r'
 int n_of_chars_inbetween_parentheses(char *entrypoint, char direction);
